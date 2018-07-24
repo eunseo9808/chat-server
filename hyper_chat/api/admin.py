9087ec5f66401a53753b8e60c60aeef696b8b3ed
eunseo Lee <eunseo9808@naver.com>
2018-07-24 18:39:28 +0900

    DB Design

--- hyper_chat/api/admin.py
+++ hyper_chat/api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Chatter
 
 # Register your models here.
+admin.site.register(Chatter)
--- hyper_chat/api/models.py
+++ hyper_chat/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Chatter(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=100, default="Chatter")
+
+    def __str__(self):
+        return self.user.__str__()
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Chatter.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.chatter.save()
+
+
+class ChatRoom(models.Model):
+    owner = models.ForeignKey(Chatter, related_name="owner_chat_rooms", on_delete=models.CASCADE)
+    opponent = models.ForeignKey(Chatter, related_name="opponent_chat_rooms", on_delete=models.CASCADE)
+    create_time = models.DateTimeField(auto_now_add=True)
+    last_chat_time = models.DateTimeField(auto_now_add=True)
+
+
+class Chat(models.Model):
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(Chatter, related_name='sender_chats', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(Chatter, related_name='receiver_chats', on_delete=models.CASCADE)
+    content = models.TextField(null=False)
+    create_time = models.DateTimeField(auto_now_add=True)
